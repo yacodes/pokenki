d1c2b93b5e0234b09431444b09392cf89774196d
Aleksandr Yakunichev <hi@ya.codes>
2022-09-30 15:48:46 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Pokenki
+
+A tool to autotranslate selected words from KOReader `vocabulary_builder` SQlite3 database, translate them and write a `.csv` file to the Desktop.
+
+Uses [lingva-scraper](https://github.com/thedaviddelta/lingva-scraper) as the main translation tool.
+
+## Dismissed tools
+- [DeepL](https://www.deepl.com) — asks for a credit card to obtain a free API key, unacceptable;
+- [Pons](https://en.pons.com/translate) — maybe? returns weird strings with HTML, which one need to clean up;
+- [LibreTranslate](https://libretranslate.com/) — self-hosted solution is cool but the quality of translations are very poor unfortunately;
+- Microsoft, Google, Amazon etc — no corporate solutions are acceptable.
---
+++ index.ts
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+
+import fs from 'fs';
+import sqlite3 from 'sqlite3';
+import { getTranslationInfo, getTranslationText } from 'lingva-scraper';
+
+const VOCABULARY_PATH = '/home/ya/Public/Devices/Poke/KOReader/settings/vocabulary_builder.sqlite3';
+
+const expandWord = (str: string): string => str.split('').join(' ');
+const replaceVowels = (str: string): string => str.replace(/(a|e|i|o|u)/g, '_');
+
+const db = new sqlite3.Database(VOCABULARY_PATH, sqlite3.OPEN_READONLY, (err) => {
+  if (err) return console.error(err);
+
+  db.all('SELECT * FROM vocabulary;', async (err, items: { word: string }[]) => {
+    if (err) return console.error(err);
+    const words: string[] = [];
+    for (let i = items.length - 1; i > 0; i--) {
+      const source = items[i].word.toLowerCase();
+      const [result, info] = [
+        // @ts-expect-error Something wrong with typings in lingva-scraper package
+        await getTranslationText('en', 'ru', source),
+        // @ts-expect-error Something wrong with typings in lingva-scraper package
+        await getTranslationInfo('en', 'ru', source),
+      ];
+      if (result) {
+        console.log(`${i}/${items.length - 1}: ${source} -> ${result}`);
+        words.push(
+          [source, result, info?.extraTranslations[0]?.type || 'unknown', replaceVowels(expandWord(source))].join(' = ')
+        );
+      }
+    }
+    fs.writeFileSync('/home/ya/Desktop/anki.en.csv', words.join('\n'));
+  });
+});
